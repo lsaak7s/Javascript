e15961bf9e1fe5428f9b392946f68d909e0e7fb2
IsaacAlves <isaacsantos.7r@gmail.com>
2026-07-16 16:52:15 -0300

    a

--- azul/script.js
+++ azul/script.js
@@ -13,37 +13,37 @@ const isaac = {
 
 }
 isaac.myName = "Isaac7"
-console.log(isaac) */
+console.log(isaac)
 
 
 
-/*
+
 const user = [
-{ value: 10, myAge: 17, myName: "Isaac" },
-{ value: 20, myAge: 18, myName: "John" },
-{ value: 30, myAge: 19, myName: "Jane" },
-{ value: 40, myAge: 20, myName: "Bob" },
-{ value: 50, myAge: 21, myName: "Alice" },
-{ value: 60, myAge: 22, myName: "Charlie" }
+    { value: 10, myAge: 17, myName: "Isaac" },
+    { value: 20, myAge: 18, myName: "John" },
+    { value: 30, myAge: 19, myName: "Jane" },
+    { value: 40, myAge: 20, myName: "Bob" },
+    { value: 50, myAge: 21, myName: "Alice" },
+    { value: 60, myAge: 22, myName: "Charlie" }
 
 ];
 
-console.log(user[1]); */
+console.log(user[1]);
 
 
 
 
 
-/*
+
 const media = 7
 const notaAluno = 7
 
 if (media >= notaAluno) {
-console.log("Aprovado")
+    console.log("Aprovado")
 }
 
 else {
-console.log("Reprovado")
+    console.log("Reprovado")
 }
 
 
@@ -51,41 +51,41 @@ const senha = "123456"
 const senhaDigitada = "123456"
 
 if (senha == senhaDigitada) {
-console.log("Senha correta")
+    console.log("Senha correta")
 } else {
-console.log("Senha incorreta")
-}*/
+    console.log("Senha incorreta")
+}
 
 
 
 
-/*
+
 function HelloWorld() {
 
     console.log("Hello World")
 
 }
 
-HelloWorld() */
+HelloWorld()
 
 
 
 
-/*
+
 const azul123 = document.querySelector("input")
 azul123.placeholder = "Digite algo aqui..."
 azul123.value = "Digite algo aqui........................"
 console.log(azul123.placeholder)
-*/
 
 
-/*
+
+
 const botton = document.querySelector("#azul123")
 
 button.style.backgroundColor = "yellow"
-*/
 
-/*
+
+
 function alertbutton(button) {
     alert("Button clicked!")
 
@@ -104,19 +104,35 @@ function aperteiNoButton() {
 
 const select = document.querySelector("#select")
 
-select.addEventListener("change", function() {
-    console.log(select.value)})*/
+select.addEventListener("change", function () {
+    console.log(select.value)
+})
 
-/*const value = Math.pow(5, 1)
-console.log(value)*/
-/* const value = Math.sqrt(125,5)
- console.log(value)*/
-/*  const value = Math.PI
-  console.log(value)*//*
+const value = Math.pow(5, 1)
+console.log(value)
+const value = Math.sqrt(125, 5)
+console.log(value)
+const value = Math.PI
+console.log(value)
 const value = Math.ceil(5.1)
-console.log(value)*//*
+console.log(value)
 const value = Math.floor(5.1)
-console.log(value)*//*
+console.log(value)
 const value = Math.random()
-console.log(value)*/
+console.log(value)
 
+
+let result = 10 + 5
+result= 70
+console.log(result)
+
+const p = 10
+const o = 20
+
+if (p !== o) {
+    console.log("p é diferente de o")
+}
+else{
+    console.log("p é igual a o")
+}
+*/
