2714fbbd9d34a60b0ef8251d7d8da0758b46666e
Isaac Alves <Isaacsantos.7r@gmail.com>
2026-08-10 20:46:43 -0300

    map.reduce.filter

--- azul/script.js
+++ azul/script.js
@@ -394,7 +394,25 @@ const newList = list.filter((value) => {
         return false
     }
 })
-console.log(newList)*/
+console.log(newList)
+Valorizadas
+const companies = [
+    { name: 'Samsung', marketValue: 50, CEO: 'Kim Hyun Suk', foundedOn: 1938 },
+    { name: 'Microsoft', marketValue: 415, CEO: 'Satya Nadella', foundedOn: 1975 },
+    { name: 'Intel', marketValue: 117, CEO: 'Brian Krzanich', foundedOn: 1968 },
+    { name: 'Facebook', marketValue: 383, CEO: 'Mark Zuckerberg', foundedOn: 2004 },
+    { name: 'Spotify', marketValue: 30, CEO: 'Daniel Ek', foundedOn: 2006 },
+    { name: 'Apple', marketValue: 845, CEO: 'TimCook', foundedOn: 1976 }
+];
+
+const marketValueOldCompany = companies.map(Company => {
+     Company.marketValue = Company.marketValue + (Company.marketValue / 10)
+    return Company
+})
+.filter(Company => Company.foundedOn < 1990)
+//.reduce((acc, Company) => acc + Company.marketValue, 0)
+
+const old = marketValueOldCompany
 
 const companies = [
     { name: 'Samsung', marketValue: 50, CEO: 'Kim Hyun Suk', foundedOn: 1938 },
@@ -406,10 +424,11 @@ const companies = [
 ];
 
 const marketValueOldCompany = companies.map(Company => {
-    Company.marketValue = Company.marketValue + (Company.marketValue / 10)
+     Company.marketValue = Company.marketValue - (Company.marketValue / 10)
     return Company
-}).filter(Company => Company.foundedOn < 1990 )
+})
+.filter(Company => Company.foundedOn < 1980)
+//.reduce((acc, Company) => acc + Company.marketValue, 0)
 
 const old = marketValueOldCompany
-
-console.table(old)
+console.table(old)*/
